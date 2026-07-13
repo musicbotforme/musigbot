14bff11dae92ee013a203542571aac6f4e9d3ae2
musicbotforme <camvax56@gmail.com>
2026-07-13 19:24:39 +0400

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 import yt_dlp
 
-TOKEN = "7434822725:AAHdA7V5QGnX6LZrLaANMrgBOP6PQSsTgbU"
+TOKEN = "7434822725:AAFUaXYs84seAvV9gOex1OBjuOuuwKVuTxk"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
